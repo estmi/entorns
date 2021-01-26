ba31382bf242fdc7943b9a8116ab5f03f4cc7d3c
estmi <a2006270@institutmontilivi.cat>
2021-01-26 18:48:23 +0100

    Versio inicial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Descodificador
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i, c = 0, a;
+			string f = null;
+			Console.WriteLine("Vols codificar o descodificar? (c/d)");
+			while (f != "c" && f != "d")
+			{
+				f = Console.ReadLine();
+			}
+			Console.WriteLine("Introdueix el texte:");
+			string t = Console.ReadLine();
+			Console.WriteLine("Introdueix la clau: (un número entre 1 i 9)");
+			while (c < 1 || c > 9)
+			{
+				c = Convert.ToInt32(Console.ReadLine());
+			}
+			if (f == "c")
+			{
+				for (i = 0; i < t.Length; i++)
+				{
+					a = t[i];
+					Console.Write((char)(a + c));
+				}
+			}
+			else
+			{
+				c = c * -1;
+				for (i = 0; i < t.Length; i++)
+				{
+					a = t[i];
+					Console.Write((char)(a + c));
+				}
+			}
+		}
+	}
+}
