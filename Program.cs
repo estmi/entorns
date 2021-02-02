e856074b546b873ebf3625d526ae6775a4baa38d
estmi <a2006270@institutmontilivi.cat>
2021-02-02 18:59:29 +0100

    Varaibles refactoritzades

--- Program.cs
+++ Program.cs
@@ -5,36 +5,36 @@ namespace Descodificador
 	{
 		static void Main(string[] args)
 		{
-			int i, c = 0, a;
-			string f = null; //que farà el programa, codificar o descodificar Esteve Miquel Guitart
+			int clau = 0, codiAscii;
+			string funcio = null; //que farà el programa, codificar o descodificar Esteve Miquel Guitart
 			Console.WriteLine("Vols codificar o descodificar? (c/d)");
 			//Console.WriteLine("Hola, fet amb linux. I ara amb WindowsVM");
-			while (f != "c" && f != "d")//validacio d'entrada
+			while (funcio != "c" && funcio != "d")//validacio d'entrada
 			{
-				f = Console.ReadLine();
+				funcio = Console.ReadLine();
 			}
 			Console.WriteLine("Introdueix el texte:");//texte a codificar o descodificar
 			string t = Console.ReadLine();
 			Console.WriteLine("Introdueix la clau: (un número entre 1 i 9)");//clau de codificacio
-			while (c < 1 || c > 9)//validacio d'entrada
+			while (clau < 1 || clau > 9)//validacio d'entrada
 			{
-				c = Convert.ToInt32(Console.ReadLine());
+				clau = Convert.ToInt32(Console.ReadLine());
 			}
-			if (f == "c")//Aplicar la clau si es de codificar o descodificar s'haplica al codi ascii la suma de la clau
+			if (funcio == "c")//Aplicar la clau si es de codificar o descodificar s'haplica al codi ascii la suma de la clau
 			{
-				for (i = 0; i < t.Length; i++)
+				for (int i = 0; i < t.Length; i++)
 				{
-					a = t[i];
-					Console.Write((char)(a + c));
+					codiAscii = t[i];
+					Console.Write((char)(codiAscii + clau));
 				}
 			}
 			else
 			{
-				c = c * -1;
-				for (i = 0; i < t.Length; i++)
+				clau = clau * -1;
+				for (int i = 0; i < t.Length; i++)
 				{
-					a = t[i];
-					Console.Write((char)(a + c));
+					codiAscii = t[i];
+					Console.Write((char)(codiAscii + clau));
 				}
 			}
 		}
