be624697e6060e3703c66626a098cf60adba4eae
estmi <a2006270@institutmontilivi.cat>
2021-02-02 18:56:32 +0100

    Versio amb comments

--- Program.cs
+++ Program.cs
@@ -6,21 +6,21 @@ namespace Descodificador
 		static void Main(string[] args)
 		{
 			int i, c = 0, a;
-			string f = null; //que farà el programa, codificar o descodificar
+			string f = null; //que farà el programa, codificar o descodificar Esteve Miquel Guitart
 			Console.WriteLine("Vols codificar o descodificar? (c/d)");
-			Console.WriteLine("Hola, fet amb linux. I ara amb WindowsVM");
-			while (f != "c" && f != "d")
+			//Console.WriteLine("Hola, fet amb linux. I ara amb WindowsVM");
+			while (f != "c" && f != "d")//validacio d'entrada
 			{
 				f = Console.ReadLine();
 			}
-			Console.WriteLine("Introdueix el texte:");
+			Console.WriteLine("Introdueix el texte:");//texte a codificar o descodificar
 			string t = Console.ReadLine();
-			Console.WriteLine("Introdueix la clau: (un número entre 1 i 9)");
-			while (c < 1 || c > 9)
+			Console.WriteLine("Introdueix la clau: (un número entre 1 i 9)");//clau de codificacio
+			while (c < 1 || c > 9)//validacio d'entrada
 			{
 				c = Convert.ToInt32(Console.ReadLine());
 			}
-			if (f == "c")
+			if (f == "c")//Aplicar la clau si es de codificar o descodificar s'haplica al codi ascii la suma de la clau
 			{
 				for (i = 0; i < t.Length; i++)
 				{
